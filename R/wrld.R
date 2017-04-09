7a7a4c344f0de51d85995a9d77893a24f7e23417
jn <tupiszakaczki@gmail.com>
2017-04-09 12:22:55 -0400

    wrld data added

---
+++ R/wrld.R
@@ -0,0 +1,37 @@
+#' @name wrld
+#' @aliases wrld
+#' @title World country polygons
+#'
+#' @description The object loaded is a \code{sf} object containing a world map data from Natural Earth with a few variables from World Bank
+#' 
+#' @format Formal class 'sf' [package "sf"]; the data contains a data.frame with 177 obs. of 11 variables:
+#' \itemize{
+#'     \item{iso_a2} {factor of ISO 2 character country codes}
+#'     \item{name_long} {factor of country names}
+#'     \item{continent} {factor of continent names}
+#'     \item{region_un} {factor of region names}
+#'     \item{subregion} {factor of subregion names}
+#'     \item{type} {factor of type names}
+#'     \item{area_km2} {integer vector of area values}
+#'     \item{pop} {integer vector of population in 2014}
+#'     \item{lifeExp} {integer vector of life expectancy at birth in 2014}
+#'     \item{gdpPercap} {integer vector of per-capita GDP in 2014}
+#'     \item{geom} {sfc_MULTIPOLYGON}
+#' }
+#' The object is in geographical coordinates using the WGS84 datum.
+#' 
+#' @source \url{http://www.naturalearthdata.com/}
+#' @source \url{http://data.worldbank.org/}
+#' @docType data
+#' @keywords datasets
+#' 
+#' @examples 
+#' library(sf)
+#' data(wrld)
+#' # or
+#' wrld <- st_read(system.file("shapes/wrld.gpkg", package="spData"))
+#' 
+#' plot(wrld)
+#'
+
+NULL
