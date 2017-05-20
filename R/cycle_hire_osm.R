e511d2e5f1f5743b136c2cc735ea328b5b0f8088
Robin Lovelace <rob00x@gmail.com>
2017-05-20 08:27:19 +0100

    Document cycle hire points - see robinlovelace/geocompr#10

--- R/cycle_hire_osm.R
+++ R/cycle_hire_osm.R
@@ -1,20 +1,21 @@
 #' @name cycle_hire_osm
 #' @aliases cycle_hire_osm
-#' @title TITLE
+#' @title Cycle hire points in London from OSM
 #'
-#' @description DESCRIPTION
+#' @description Dataset downloaded using the osmdata package
+#' representing cycle hire points accross London.
 #' 
-#' @format FORMAT:
+#' @format 
 #' \itemize{
-#'     \item{osm_id} {}
-#'     \item{name} {}
-#'     \item{capacity} {}
-#'     \item{cyclestreets_id} {}
-#'     \item{description} {}
+#'     \item{osm_id} {The OSM ID}
+#'     \item{name} {The name of the cycle point}
+#'     \item{capacity} {How many bikes it can take}
+#'     \item{cyclestreets_id} {The ID linked to cyclestreets' photomap}
+#'     \item{description} {Additional description of points}
 #'     \item{geometry} {sfc_POINT}
 #' }
 #' 
-#' @source \url{http://SOURCE.com}
+#' @source \url{http://www.openstreetmap.org}
 #' @docType data
 #' @keywords datasets sf
 #' 
@@ -25,6 +26,17 @@
 #' cycle_hire_osm <- st_read(system.file("shapes/cycle_hire_osm.geojson", package="spData"))
 #' 
 #' plot(cycle_hire_osm)
-#'
+#' 
+#' # Code used to download the data:
+#' \dontrun{
+#' library(osmdata)
+#' library(sf)
+#' q = add_feature(opq = opq("London"), key = "network", value = "tfl_cycle_hire")
+#' lnd_cycle_hire = osmdata_sf(q)
+#' lnd_cycle_p = lnd_cycle_hire$osm_points
+#' plot(lnd_cycle_p)
+#' lnd_cycle_p_mini = dplyr::select(lnd_cycle_p, osm_id, name, capacity, cyclestreets_id, description)
+#' nrow(lnd_cycle_p_mini)
+#' }
 
 NULL
