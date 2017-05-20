8adae05d14094f55b6f6f8bc071cfc2bd29360ec
Robin Lovelace <rob00x@gmail.com>
2017-05-20 08:46:18 +0100

    Document cycle hire data

--- R/cycle_hire.R
+++ R/cycle_hire.R
@@ -1,20 +1,23 @@
 #' @name cycle_hire
 #' @aliases cycle_hire
-#' @title TITLE
+#' @title Cycle hire points in London
 #'
-#' @description DESCRIPTION
+#' @description 
+#' 
+#' Points
+#' representing cycle hire points accross London. 
 #' 
 #' @format FORMAT:
 #' \itemize{
-#'     \item{id} {}
-#'     \item{name} {}
-#'     \item{area} {}
-#'     \item{nbikes} {}
-#'     \item{nempty} {}
+#'     \item{id} {Id of the hire point}
+#'     \item{name} {Name of the point}
+#'     \item{area} {Area they are in}
+#'     \item{nbikes} {The number of bikes currently parked there}
+#'     \item{nempty} {The number of empty places}
 #'     \item{geometry} {sfc_POINT}
 #' }
 #' 
-#' @source \url{http://SOURCE.com}
+#' @source \url{cyclehireapp.com/cyclehirelive/cyclehire.csv}
 #' @docType data
 #' @keywords datasets sf
 #' 
@@ -25,6 +28,13 @@
 #' cycle_hire <- st_read(system.file("shapes/cycle_hire.geojson", package="spData"))
 #' 
 #' plot(cycle_hire)
-#'
+#' 
+#' \dontrun{
+#' # Download the data
+#' cycle_hire = readr::read_csv("http://cyclehireapp.com/cyclehirelive/cyclehire.csv", col_names = FALSE, skip = TRUE)
+#' cycle_hire = cycle_hire[c_names]
+#' c_names = c("id", "name", "area", "lat", "lon", "nbikes", "nempty")
+#' cycle_hire = st_sf(cycle_hire, st_multipoint(c_names[c("lon", "lat")]))
+#' }
 
 NULL
