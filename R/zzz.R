eba2fca19bc85b58a6a75fd2439d8d035edd50ae
Robin <Robinlovelace@users.noreply.github.com>
2017-08-22 08:01:35 +0100

    Update startup message

--- R/zzz.R
+++ R/zzz.R
@@ -11,23 +11,10 @@
 
 .onAttach <- function(libname, pkgname) {
         if (!.pkgenv$has_data) {
-                # msg <- paste("To use this package, you must install the",
-                #              "spDataLarge package. To install that ",
-                #              "package, run `install.packages('spDataLarge',",
-                #              "repos='https://nowosad.github.io/drat/', type='source')`.")
-                msg <- paste("To use this package, you must install the",
-                             "spDataLarge package. To install that ",
-                             "package, run `install.packages('spDataLarge')`.")
+                msg <- paste("To access larger datasets in this package, install the",
+                             "spDataLarge package with:\n",
+                             "`install.packages('spDataLarge')`")
                 msg <- paste(strwrap(msg), collapse="\n")
                 packageStartupMessage(msg)
         }
 }
-
-# hasData <- function(has_data = .pkgenv$has_data) {
-#         if (!has_data) {
-#                 msg <- paste("To use this function, you must have the",
-#                              "`spDataLarge` package installed.")
-#                 msg <- paste(strwrap(msg), collapse="\n")
-#                 stop(msg)
-#         }
-# }
