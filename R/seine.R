ab3a264721d02e6d54cdb6b3be5c918b93d92ec9
jn <tupiszakaczki@gmail.com>
2017-12-05 08:41:15 -0500

    seine added and lnd.geojson removed

---
+++ R/seine.R
@@ -0,0 +1,41 @@
+#' @name seine
+#' @aliases rivers
+#' @title Small river network in France
+#'
+#' @description 
+#' 
+#' Lines representing the Seine, Marne and Yonne rivers.
+#' 
+#' @format FORMAT:
+#' \itemize{
+#'     \item{name} {name}
+#'     \item{geometry} {sfc_MULTILINESTRING}
+#' }
+#' The object is in the RGF93 / Lambert-93 CRS.
+#' 
+#' @seealso 
+#' See the rnaturalearth package: https://cran.r-project.org/package=rnaturalearth
+#' 
+#' @source \url{http://www.naturalearthdata.com/}
+#' 
+#' @docType data
+#' @keywords datasets sf
+#' 
+#' @examples 
+#' if (requireNamespace("sf", quietly = TRUE)) {
+#'   library(sf)
+#'   seine
+#'   plot(seine)
+#' }
+#' \dontrun{
+#' library(sf)
+#' library(rnaturalearth)
+#' library(tidyverse)
+#' 
+#' seine = ne_download(scale = 10, type = "rivers_lake_centerlines", 
+#'                     category = "physical", returnclass = "sf") %>% 
+#'         filter(name %in% c("Yonne", "Seine", "Marne")) %>% 
+#'         select(name = name_en) %>% 
+#'         st_transform(2154)
+#' }
+"seine"
