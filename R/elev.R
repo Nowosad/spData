0b8125164d2eec9efa15bb259c13bce151590514
jannes-m <malnamalja@gmx.de>
2017-12-22 14:28:00 +0100

    adding elev and grain

---
+++ R/elev.R
@@ -0,0 +1,13 @@
+#' @name elev
+#' @title Artificial toy raster data set representing elevation
+#'
+#' @description The raster data represents elevation in meters and uses WGS84 as
+#'   a coordinate reference system.
+#' @docType data
+#' @keywords datasets raster
+#' 
+#' @examples 
+#' library(raster)
+#' data(elev)
+#' 
+"elev"
---
+++ R/grain.R
@@ -0,0 +1,13 @@
+#' @name grain
+#' @title Artificial toy raster data set representing grain sizes
+#' @description The ratified raster data represents grain sizes with the the
+#'   three classes clay, silt and sand with WGS84 as a coordinate reference
+#'   system.
+#' @docType data
+#' @keywords datasets raster
+#' 
+#' @examples 
+#' library(raster)
+#' data(grain)
+#' 
+"grain"
