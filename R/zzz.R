8b290959790a3c62d55ec8763c4b01e081298a22
jn <tupiszakaczki@gmail.com>
2018-02-17 10:58:28 -0500

    invisible action removed

--- R/zzz.R
+++ R/zzz.R
@@ -1,20 +1,30 @@
-.pkgenv <- new.env(parent=emptyenv())
+.pkgenv <- new.env(parent = emptyenv())
 
 .onLoad  <- function(libname, pkgname) {
         has_data <- requireNamespace("spDataLarge", quietly = TRUE)
         .pkgenv[["has_data"]] <- has_data
-        repos = getOption("repos")
-        repos["nowosaddrat"] = "https://nowosad.github.io/drat/"
-        options(repos = repos)
-        invisible(repos)
+        # repos = getOption("repos")
+        # repos["nowosaddrat"] = "https://nowosad.github.io/drat/"
+        # options(repos = repos)
+        # invisible(repos)
 }
 
 .onAttach <- function(libname, pkgname) {
         if (!.pkgenv$has_data) {
                 msg <- paste("To access larger datasets in this package, install the",
                              "spDataLarge package with:\n",
-                             "`install.packages('spDataLarge')`")
+                             "`install.packages('spDataLarge',", 
+                             "repos='https://nowosad.github.io/drat/', type='source'))`")
                 msg <- paste(strwrap(msg), collapse="\n")
                 packageStartupMessage(msg)
+        }
+}
+
+hasData <- function(has_data = .pkgenv$has_data) {
+        if (!has_data) {
+                msg <- paste("To use this function, you must have the",
+                             "`spDataLarge` package installed.")
+                msg <- paste(strwrap(msg), collapse="\n")
+                stop(msg)
         }
 }
