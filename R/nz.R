2ec25223fcba396efc1f3749fa44572ff5334266
Robin Lovelace <rob00x@gmail.com>
2018-04-12 17:24:02 +0100

    Update nz code + add new vars

--- R/nz.R
+++ R/nz.R
@@ -29,18 +29,34 @@
 #'   plot(nz)
 #' }
 #' \dontrun{
-#' u_nz = paste0("http://www3.stats.govt.nz/digitalboundaries/annual/",
-#'   "ESRI_Geodatabase_2017_Digital_Boundaries_Generalised_Clipped.zip")
-#' download.file(u_nz, destfile = "nz.zip")
-#' unzip("nz.zip")
-#' st_layers("2017 Digital Boundaries Generalised Clipped.gdb/")
-#' nz = st_read("2017 Digital Boundaries Generalised Clipped.gdb", layer = "REGC2017_GV_Clipped")
-#' object.size(nz) / 1000000 # ~15 MB
-#' nz = rmapshaper::ms_simplify(nz, keep = 0.001)
-#' object.size(nz) / 1000 # 41 KB!
-#' nz = nz_orig[1:16, ] # remove areas outside nz territory
-#' # Tidy-up
-#' unlink("2017 Digital Boundaries Generalised Clipped.gdb/", recursive = TRUE)
-#' unlink("nz.zip")
+#' # Find "Regional Council 2016 Generalised Version"
+#' # select the GeoPackage option in the "Vectors/tables" dropdown
+#' # at https://datafinder.stats.govt.nz/data/ (requires registration)
+#' # Save the result as:
+#' # "statsnzregional-council-2017-generalised-version-GPKG.zip"
+#' unzip("statsnzregional-council-2017-generalised-version-GPKG.zip")
+#' library(sf)
+#' library(tidyverse)
+#' nz_full = st_read("regional-council-2017-generalised-version.gpkg")
+#' object.size(nz_full) / 1000000 # ~2 MB
+#' nz = rmapshaper::ms_simplify(nz_full, keep = 0.01)
+#' object.size(nz) / 1000 # ~40 KB
+#' names(nz)
+#' nz$REGC2017_NAME
+#' nz = filter(nz, REGC2017_NAME != "Area Outside Region") %>% 
+#'   select(Name = REGC2017_NAME, `Land area` = LAND_AREA_SQ_KM)
+#' devtools::install_github("hadley/rvest")
+#' library(rvest)
+#' doc = read_html("https://en.wikipedia.org/wiki/Regions_of_New_Zealand") %>% 
+#'         html_nodes("div table")
+#' tab = doc[[3]] %>% html_table()
+#' tab = tab %>% select(Name = Region, Population = `Population[20]`, Island)
+#' tab = tab %>% mutate(Population = str_replace_all(Population, ",", "")) %>% 
+#'         mutate(Population = as.numeric(Population)) %>% 
+#'         mutate(Name = str_remove_all(Name, " \\([1-9]\\)?.+"))
+#' nz$Name = as.character(nz$Name)
+#' nz$Name = str_remove(nz$Name, " Region")
+#' nz$Name %in% tab$Name
+#' nz = left_join(nz, tab)
 #' }
 "nz"
