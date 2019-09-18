e5f80e5c4ebca8b7efc587e686388429af150190
nowosad <dukekubas@gmail.com>
2019-09-18 08:56:04 +0200

    renames x, y, xyz to go_x, go_y, go_xyz

--- R/getisord.R
+++ R/getisord.R
@@ -1,8 +1,8 @@
 #' @name getisord
-#' @aliases getisord x y xyz
+#' @aliases getisord go_x go_y go_xyz
 #' @title Getis-Ord remote sensing example data
 #'
-#' @description The \code{xyz} data frame has 256 rows and 3 columns. Vectors \code{x} and \code{y} are of length 16 and give the centres of the grid rows and columns, 30m apart. The data start from the bottom left, Getis and Ord start from the top left - so their 136th grid cell is our 120th.
+#' @description The \code{go_xyz} data frame has 256 rows and 3 columns. Vectors \code{go_x} and \code{go_y} are of length 16 and give the centres of the grid rows and columns, 30m apart. The data start from the bottom left, Getis and Ord start from the top left - so their 136th grid cell is our 120th.
 #' 
 #' @format 
 #' This data frame contains the following columns:
@@ -18,9 +18,9 @@
 #' 
 #' @examples 
 #' data(getisord)
-#' image(x, y, t(matrix(xyz$val, nrow=16, ncol=16, byrow=TRUE)), asp=1)
-#' text(xyz$x, xyz$y, xyz$val, cex=0.7)
-#' polygon(c(195,225,225,195), c(195,195,225,225), lwd=2)
-#' title(main="Getis-Ord 1996 remote sensing data")
+#' image(go_x, go_y, t(matrix(go_xyz$val, nrow = 16, ncol=16, byrow = TRUE)), asp = 1)
+#' text(go_xyz$x, go_xyz$y, go_xyz$val, cex = 0.7)
+#' polygon(c(195, 225, 225, 195), c(195, 195, 225, 225), lwd = 2)
+#' title(main = "Getis-Ord 1996 remote sensing data")
 #'
 NULL
