d4f0feacbb4a17810ffc7f36e2901f25a273d8fb
Robin <Robinlovelace@users.noreply.github.com>
2020-02-11 09:51:04 +0000

    Update urban aglomerations example (not tested)

--- R/urban_agglomerations.R
+++ R/urban_agglomerations.R
@@ -27,15 +27,15 @@
 #' }
 #' # Code used to download the data:
 #' \dontrun{
-#' download.file(destfile = "WUP2014-F11a-30_Largest_Cities.xls",
-#' url = "https://esa.un.org/unpd/wup/CD-ROM/WUP2014_XLS_CD_FILES/WUP2014-F11a-30_Largest_Cities.xls")
+#' download.file(destfile = "WUP2018-F11b-30_Largest_Cities_in_2018_by_time.xls",
+#' url = "https://population.un.org/wup/Download/Files/WUP2018-F11b-30_Largest_Cities_in_2018_by_time.xls")
 #' library(dplyr)
 #' library(sf)
-#' urban_agglomerations = readxl::read_excel("WUP2014-F11a-30_Largest_Cities.xls", skip = 16) %>%
+#' urban_agglomerations = readxl::read_excel("WUP2018-F11b-30_Largest_Cities_in_2018_by_time.xls", skip = 16) %>%
 #'     st_as_sf(coords = c("Longitude", "Latitude"), crs = 4326)
 #' names(urban_agglomerations) <- gsub(" ", "_", tolower(names(urban_agglomerations)) ) %>% 
 #'         gsub("\\(|\\)", "", .)
 #' devtools::use_data(urban_agglomerations, overwrite = TRUE)
-#' file.remove("WUP2014-F11a-30_Largest_Cities.xls")
+#' file.remove("WUP2018-F11b-30_Largest_Cities_in_2018_by_time.xls")
 #' }
 "urban_agglomerations"
