0bcee4ac43f03790a72c7a849b59e34cdbdf11df
robinlovelace <rob00x@gmail.com>
2023-06-20 10:51:30 +0100

    Update with new fix for #64

--- data-raw/update-world.R
+++ data-raw/update-world.R
@@ -4,6 +4,7 @@
 library(mapedit)
 world = spData::world
 names(world)
+sf::st_geometry_type(world$geom) |> table()
 
 # Test 1: Edit all of the features (too slow)
 # world2 = mapedit::editFeatures(world)
@@ -22,50 +23,22 @@ plot(crimea_polygon)
 
 russia_updated = sf::st_difference(russia_polygons, crimea_polygon)
 mapview::mapview(russia_updated)
+russia_updated_union = sf::st_union(russia_updated)
+sf::st_geometry_type(russia_updated_union)
 
+ukraine_updated = sf::st_union(ukraine, crimea_polygon) |>
+  sf::st_cast("MULTIPOLYGON")
+mapview::mapview(ukraine_updated)
+sf::st_geometry_type(ukraine_updated)
 
-# Test 2: Edit only Ukraine and Russia's features
-ukr2 = mapedit::editFeatures(ukr)
-# Plot just Ukraine:
-ukr2 |>
-  subset(name_long == "Ukraine") |>
-  plot()
+sf::st_geometry_type(world$geom) |> table() # All MultiPolygon
 
-# Update geometry of Ukraine in world object:
-world_new = world
-world_new$geom[world$name_long == "Ukraine"] = ukr2$geom[ukr2$name_long == "Ukraine"]
-waldo::compare(world, world_new)
+world$geom[grepl("Russia", world$name_long)] = russia_updated_union
+# Update Ukraine:
+world$geom[grepl("Ukraine", world$name_long)] = ukraine_updated$geom
+mapview::mapview(world)
 
-# Update geometry of Russia in world object:
-russia = ukr2 |>
-  subset(grepl("Russia", name_long))
-# Fails to edit sub-components of Russia:
-russia2 = mapedit::editFeatures(russia)
-
-# Cast Russia to POLYGON:
-russia3 = russia |>
-  sf::st_cast("POLYGON")
-crimea_centroid = tmaptools::geocode_OSM("Crimea", as.sf = TRUE) 
-crimea_polygon = russia3[crimea_centroid, ]
-plot(crimea_polygon)
-# Remove Crimea from Russia:
-russia4 = russia3[crimea_centroid, , op = sf::st_disjoint]
-lengths(sf::st_intersects(russia3, crimea_centroid))
-#  [1] 0 0 0 1 0 0 0 0 0 0 0 0 0 0 
-# 4th element is Crimea
-nrow(russia3) - nrow(russia4)
-russia4_union = sf::st_union(russia4)
-# Extract Crimea from Russia and plot lines:
-plot(russia$geom[[1]][[4]][[1]], type = "l")
-# Remove Crimea from Russia:
-russia$geom[[1]][[4]] = NULL
-mapview::mapview(russia)
-
-# Update geometry of Russia in world object:
-world_new$geom[grepl("Russia", world_new$name_long)] = russia$geom
-# Check results:
-mapview::mapview(world_new)
-waldo::compare(world, world_new)
+waldo::compare(spData::world, world)
 usethis::use_data(world, overwrite = TRUE)
 
 # Test world object
